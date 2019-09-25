56c010a72f888a24c0c7fa45643a816f1e45a972
Kamila Souckova <coding@kamila.is>
2019-09-25 13:26:32 +0200

    It's working :D

--- transport-ch-exporter.py
+++ transport-ch-exporter.py
@@ -14,6 +14,9 @@ from twisted.internet import defer, endpoints, reactor, task
 import treq
 
 METRICS_PREFIX   = 'transport'
+API_BASEURL      = 'http://transport.opendata.ch/v1'
+REFRESH_INTERVAL = 10*60  # seconds
+CONNS_LIMIT      = 10     # how many connections we'll request in one go
 
 # Yes yes, I am doing the exact opposite of
 # https://prometheus.io/docs/practices/naming/#labels
@@ -22,12 +25,12 @@ METRICS_PREFIX   = 'transport'
 # many lines and thus it's fine.
 # Use the optional `links_whitelist` config option if the number of
 # metrics gets too large.
-ConnKey = namedtuple('ConnKey', ('station', 'name', 'category', 'number'))
+# ^^ UNIMPLEMENTED :D
+ConnKey = namedtuple('ConnKey', ('station', 'category', 'number', 'to'))
 NEXT_DEP = prom.Gauge('{}_next_departure'.format(METRICS_PREFIX),
                       'Time until next departure',
-                      ConnKey._fields,  # labels
-                      # unit='unixtime')
-                      unit='seconds')
+                      labelnames=ConnKey._fields,
+                      unit='unixtime')
 
 def ensure_deferred_f(f):
     @functools.wraps(f)
@@ -36,82 +39,104 @@ def ensure_deferred_f(f):
         return defer.ensureDeferred(result)
     return wrapper
 
-# class TransportExporter:
-REFRESH_INTERVAL = 10*60  # seconds
 
-CONNECTIONS = []
-# We bucket connections for the NEXT_DEPARTURE_IN thingy.
-CONN_BUCKETS = {}  # ConnKey => data
+@attr.s
+class Journey:
+    """Wraps journey data returned from the transport API"""
+    data = attr.ib()
 
-def no_walk(sections):
-    return [s for s in sections if not s['walk']]
+    @property
+    def key(self):
+        """Raises IndexError when this is only walking!"""
+        first_seg = self.get_sections()[0]
+        return ConnKey(
+            station= first_seg['departure']['station']['name'],
+            category=first_seg['journey']['category'],
+            number=  first_seg['journey']['number'],
+            to=      first_seg['journey']['to'],
+        )
 
-def get_departure(conn):
-    # return parsedate(no_walk(conn['sections'])[0]['departure']['departure'])
-    return no_walk(conn['sections'])[0]['departure']['departureTimestamp']
+    @property
+    def departure(self):
+        return datetime.fromtimestamp(self.departure_ts)
 
-# def remove_old_connections():
-    # TODO implement
-    # for c in CONNECTIONS:
+    @property
+    def departure_ts(self):
+        return self.get_sections()[0]['departure']['departureTimestamp']
 
-# TODO you don't want duplicate conns either
+    def get_sections(self, include_walk=False):
+        if include_walk:
+            return self.data['sections']
+        else:
+            return [s for s in self.data['sections'] if not s['walk']]
 
-def getkey(conn):
-    first_seg = no_walk(conn['sections'])[0]
-    return ConnKey(
-        station= first_seg['departure']['station']['name'],
-        name=    first_seg['journey']['name'],
-        category=first_seg['journey']['category'],
-        number=  first_seg['journey']['number'],
-    )
 
-def bucketise(conn):
-    key = getkey(conn)
-    CONN_BUCKETS.setdefault(key, [])
-    CONN_BUCKETS[key].append(conn)
+@attr.s
+class TransportExporter:
+    config     = attr.ib()
+    conn_times = attr.ib(factory=dict)  # ConnKey => set(departure_ts)
 
-@ensure_deferred_f
-async def get_data(reactor, config):
-    URL  = 'http://transport.opendata.ch/v1/connections?from=Zürich,%20Maienstrasse&to=Zürich%20Haldenbach&limit=10&fields[]=connections'
-    res  = await treq.get(URL)
-    data = await res.json()
-    CONNECTIONS.append(data['connections'])
-    for c in data['connections']:
-        print('Got conn: {} at {}'.format(getkey(c), datetime.fromtimestamp(get_departure(c))))
-        bucketise(c)
-    set_up_metrics()
+    @ensure_deferred_f
+    async def load_data(self, reactor):
+        requests = []
+        for conn in self.config['connections']:
+            requests.append(self.load_conns(reactor, conn['from'], conn['to']))
+        await defer.DeferredList(requests)
 
-def get_next_departure(key):
-    return min(get_departure(c) for c in CONN_BUCKETS[key])
+    @ensure_deferred_f
+    async def load_conns(self, reactor, station_from, station_to):
+        print('Looking up connnection: {} -> {}'.format(station_from, station_to))
+        res  = await treq.get(API_BASEURL+'/connections', params={
+            'from':     station_from,
+            'to':       station_to,
+            'limit':    CONNS_LIMIT,
+            'fields[]': 'connections',
+        })
+        data = await res.json()
+        for entry in data['connections']:
+            j = Journey(entry)
+            if not j.get_sections():  # skip walking-only
+                print('WARNING: Got a walk-only journey, is that expected?')
+                continue
+            print('Got journey: {} at {}'.format(j.key, j.departure))
 
-def set_up_metrics():
-    for key, conns in CONN_BUCKETS.items():
-        NEXT_DEP.labels(*key).set_function(lambda: int(get_next_departure(key)))
+            if not j.key in self.conn_times:  # we have not seen this connection yet
+                self.conn_times[j.key] = set()
+                self.setup_metrics(j.key)
+            self.conn_times[j.key].add(j.departure_ts)
 
-def start_http_server(reactor):
-    root = resource.Resource()
-    root.putChild(b'metrics', MetricsResource())
+    def get_next_departure(self, key):
+        return min(self.conn_times[key])
 
-    port = os.getenv('PORT', 8000)
-    endpoints.serverFromString(
-        reactor, r'tcp:interface=\:\:0:port={}'.format(port)
-    ).listen(server.Site(root))
-    print('HTTP server listening on port {}'.format(port))
+    def setup_metrics(self, key):
+        NEXT_DEP.labels(*key).set_function(lambda: self.get_next_departure(key))
+
+
+    def start_http_server(self, reactor):
+        root = resource.Resource()
+        root.putChild(b'metrics', MetricsResource())
+
+        port = os.getenv('PORT', 8000)
+        endpoints.serverFromString(
+            reactor, r'tcp:interface=\:\:0:port={}'.format(port)
+        ).listen(server.Site(root))
+        print('HTTP server listening on port {}'.format(port))
+
+    def start_refresh_loop(self, reactor):
+        self.refresh_loop = task.LoopingCall(self.load_data, reactor)
+        self.refresh_loop.start(REFRESH_INTERVAL)
+
+    def start(self, reactor):
+        self.start_refresh_loop(reactor)
+        self.start_http_server(reactor)
+
 
 def main():
-    # read config
-    config = None
     with open('config.yml', 'r') as f:
         config = yaml.safe_load(f)
 
-    # set up metrics
-    for station in config['stations']:
-        print(station)
-
-    refresh_loop = task.LoopingCall(get_data, reactor, config)
-    refresh_loop.start(REFRESH_INTERVAL)
-
-    start_http_server(reactor)
+    transport_exporter = TransportExporter(config)
+    transport_exporter.start(reactor)
     reactor.run()
 
 if __name__ == '__main__':
